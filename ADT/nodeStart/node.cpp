a5e8394ef9d7219d055e717192066730cfa40384
Justin_Mao <201910311127@stu.shmtu.edu.cn>
2020-10-20 23:12:23 +0800

    node

--- ADT/nodeStart/node.cpp
+++ ADT/nodeStart/node.cpp
@@ -5,13 +5,60 @@ int main()
     LinkList L = new LNode;
     int maxPlace;
     string input;
-    creatList(L, 3);
-    cout << "please add" << endl;
-    addNode(2, L);
-    cout << "please enter name" << endl;
-    cin >> input;
-    maxPlace = searchNode(input, L);
-    deleteNode(maxPlace, L);
-    searchNode(input, L);
-    return 0;
+    int init;
+    int select;
+    for (;;)
+    {
+        cout << "Welcom to Address Book of Mao" << endl;
+        cout << "1) Start my new address book" << endl;
+        cout << "2) I already have my address book" << endl;
+        cin >> select;
+        switch (select)
+        {
+        case 1:
+            cout << "Please enter how many friends you want to add to your address book." << endl;
+            cin >> init;
+            creatList(L, init);
+            break;
+
+        case 2:
+            int chose;
+            cout << "1) I want to find someone." << endl;
+            cout << "2) I want add someone." << endl;
+            cin >> chose;
+            switch (chose)
+            {
+            case 1:
+                cout << "Please enter his/her name" << endl;
+                cin >> input;
+                maxPlace = searchNode(input, L);
+                int DC;
+                cout << "Is it her/him ?" << endl;
+                cout << "Do you want to delete[1] OR change[2] ?" << endl;
+                cin >> DC;
+                switch (DC)
+                {
+                case 1:
+                    deleteNode(maxPlace, L);
+                    break;
+
+                default:
+                    deleteNode(maxPlace, L);
+                    addNode(maxPlace, L);
+                    break;
+                }
+                break;
+
+            case 2:
+                addNode(1, L);
+                break;
+
+            default:
+                break;
+            }
+
+        default:
+            break;
+        }
+    }
 }
--- Cpp/labReport/group4/exp1.cpp
+++ Cpp/labReport/group4/exp1.cpp
@@ -1,19 +1,22 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-int lenString(string s){
+/*int lenString(string s)
+{
     int i = 1;
-    while (s[i]!=0)
+    while (s[i] != 0)
     {
         i++;
     }
     return i;
-}
+}*/
 
-int main(){
+int main()
+{
     string s;
     cout << "Please enter string" << endl;
     cin >> s;
-    cout << "The length is " << lenString(s) << endl;
+    cout << "The length is " << s.length() << endl;
     return 0;
 }
